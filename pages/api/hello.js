54fb00d45cbf82398b0b63dae8f0ee7652de708d
haru <haruki1472@icloud.com>
2022-09-02 13:01:13 +0900

    データの受け口作成

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  //status 200は正常終了の意味
+  res.status(200).json({ message: "こんにちは", name: "春希" });
 }
