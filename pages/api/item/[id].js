cf9a6e2abd4f36683cff1f32ececd473a1f1086a
haru <haruki1472@icloud.com>
2022-09-19 11:42:53 +0900

    データ読み取り(単体)成功

--- pages/api/item/[id].js
+++ pages/api/item/[id].js
@@ -1,18 +1,28 @@
-import connectDB from "../utils/datebase";
-import { ItemModel } from "../utils/schemeModels";
+import mongoose from "mongoose";
+import connectDB from "../../../utils/datebase";
+import { ItemModel } from "../../../utils/schemeModels";
 
 const getSigleItem = async (req, res) => {
   console.log("-- --??.jsです-- --");
   try {
     await connectDB();
     console.log("-----------------------------success-----------");
-    console.log(req);
-    const singleItem = await ItemModel.findById(req.query.id);
-    console.log("成功");
-    return res.status(200).send({
-      message: "アイテム読み取り成功(シングル画像ここ)",
-      singleItem: singleItem,
+    console.log(req.query.id);
+
+    if (req.query.id.match(/^[0-9a-fA-F]{24}$/)) {
+      console.log("成功");
+      const singleItem = await ItemModel.findById(req.query.id);
+      console.log(singleItem);
+      return res.status(200).json({
+        message: "アイテム読み取り成功(シングル画像ここ)",
+        singleItem: singleItem,
+      });
+    }
+    console.log("不適切");
+    return res.status(400).send({
+      message: "アイテム読み取りIDが不適切",
     });
+    console.log("成功");
   } catch (err) {
     console.log("-- -- -- err -- -- --");
     console.log(err);
