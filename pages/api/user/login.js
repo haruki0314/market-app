3f665adf692a74ff0fbd5c29385eb7031e04fb14
haru <haruki1472@icloud.com>
2022-09-22 00:37:02 +0900

    login機能エラー

--- pages/api/user/login.js
+++ pages/api/user/login.js
@@ -4,11 +4,29 @@ import { UserModel } from "../../../utils/schemeModels";
 const loginUser = async (req, res) => {
   try {
     await connectDB();
+    const savedUserData = await UserModel.findOne({ email: req.body.email });
     console.log(req.body.email);
-    const sagedUserData = await UserModel.findOne({ email: req.body.email });
-    console.log(sagedUserData);
-    return res.staus(200).json({ message: "ログイン成功" });
-  } catch {
+    console.log(savedUserData);
+    if (savedUserData) {
+      console.log("一個め");
+      console.log(`${req.body.password}--${savedUserData.password}`);
+      const a = req.body.password;
+      const b = savedUserData.password;
+      console.log("これ");
+      if (a === b) {
+        return res.staus(200).json({ message: "ログイン成功" });
+      } else {
+        return res
+          .status(400)
+          .json({ message: "ログイン失敗：パスワードが間違い" });
+      }
+    } else {
+      return res
+        .status(400)
+        .json({ message: "ログイン失敗：ユーザーを登録してください" });
+    }
+  } catch (err) {
+    console.log("エラ〜発生");
     return res.status(400).json({ message: "ログイン失敗" });
   }
 };
