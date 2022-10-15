f94986185b265ff1234252c3afa2dd5444ad3a7c
haru <haruki1472@icloud.com>
2022-10-15 23:57:20 +0900

    修正 やっと動いた

--- utils/auth.js
+++ utils/auth.js
@@ -1,30 +1,32 @@
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 
-const secret_key = "nextmarket"
+const secret_key = "nextmarket";
 
 const auth = (handler) => {
-    return async(req, res) => {
-        if(req.method === "GET"){
-            return handler(req, res)
-        }
-
-        const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImhlbGxvQG1vbm90ZWluLmNvbSIsImlhdCI6MTY1MDYwNzMxMCwiZXhwIjoxNjUwNjkwMTEwfQ.lX7ZEuaiI_4b20X5E1kfwYkv6ofCti3dHDZ2oS1EwhE"
-        
-        //await req.headers.authorization.split(" ")[1]
-        
-        if(!token){
-            return res.status(401).json({message: "トークンがありません"})
-        }
-
-        try{
-            const decoded = jwt.verify(token, secret_key)
-            req.body.email = decoded.email 
-            return handler(req, res)
-        }catch(err){
-            return res.status(401).json({message: "トークンが正しくないので、ログインしてください"})
-        } 
-    
+  return async (req, res) => {
+    if (req.method === "GET") {
+      return handler(req, res);
     }
-}
 
-export default auth
+    const token =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFAY28uanAiLCJpYXQiOjE2NjU4NDQ4OTgsImV4cCI6MTY2NTkyNzY5OH0.VQmDLCoQVhQOgJfSzcJi-NaCS5Impe0ftQ4X3xG3bic";
+
+    //await req.headers.authorization.split(" ")[1]
+
+    if (!token) {
+      return res.status(401).json({ message: "トークンがありません" });
+    }
+
+    try {
+      const decoded = jwt.verify(token, secret_key);
+      req.body.email = decoded.email;
+      return handler(req, res);
+    } catch (err) {
+      return res
+        .status(401)
+        .json({ message: "トークンが正しくないので、ログインしてください" });
+    }
+  };
+};
+
+export default auth;
--- utils/database.js
+++ utils/database.js
@@ -1,13 +1,15 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
-const connectDB = async() => {
-    try{
-        await mongoose.connect("mongodb+srv://monotein:xxxx@cluster0.oskvj.mongodb.net/appDataBase?retryWrites=true&w=majority")
-        console.log("Success: Connected to MongoDB")
-    }catch(err){
-        console.log("Failure: Unconnected to MongoDB")
-        throw new Error()
-    }
-}
+const connectDB = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://haru:Ha12ru34@cluster0.pzm924t.mongodb.net/?retryWrites=true&w=majority"
+    );
+    console.log("Success: Connected to MongoDB");
+  } catch (err) {
+    console.log("Failure: Unconnected to MongoDB");
+    throw new Error();
+  }
+};
 
-export default connectDB
+export default connectDB;
