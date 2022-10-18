a98959c551844b6c9c0d72118c21b3a794986d3c
haru <haruki1472@icloud.com>
2022-10-19 00:13:32 +0900

    修正　アカウント登録API機能

--- pages/user/register.js
+++ pages/user/register.js
@@ -1,18 +1,19 @@
-import { JsonWebTokenError } from "jsonwebtoken";
 import { useState } from "react";
+import Head from "next/head";
+
 const Register = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  console.log(name);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       const response = await fetch("http://localhost:3000/api/user/register", {
         method: "POST",
         headers: {
-          Accepst: "application/json",
-          "Contet-Type": "application/json",
+          Accept: "application/json",
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
           name: name,
@@ -23,42 +24,36 @@ const Register = () => {
       const jsonData = await response.json();
       alert(jsonData.message);
     } catch (err) {
-      alert("登録失敗");
+      alert("ユーザー登録失敗");
     }
   };
   return (
     <div>
-      <h1>ユーザー登録</h1>
+      <Head>
+        <title>ユーザー登録</title>
+      </Head>
+      <h1 className="page-title">ユーザー登録</h1>
       <form onSubmit={handleSubmit}>
         <input
           value={name}
-          onChange={(e) => {
-            setName(e.target.value);
-            console.log(e);
-          }}
+          onChange={(e) => setName(e.target.value)}
           type="text"
           name="name"
           placeholder="名前"
           required
         />
         <input
-          onChange={(e) => {
-            console.log(e);
-            setEmail(e.target.value);
-          }}
-          type="text"
           value={email}
-          name="password"
+          onChange={(e) => setEmail(e.target.value)}
+          type="text"
+          name="email"
           placeholder="メールアドレス"
           required
         />
         <input
-          onChange={(e) => {
-            console.log(e);
-            setPassword(e.target.value);
-          }}
-          type="text"
           value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="text"
           name="password"
           placeholder="パスワード"
           required
@@ -68,4 +63,5 @@ const Register = () => {
     </div>
   );
 };
+
 export default Register;
